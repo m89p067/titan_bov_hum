855e4c113ee3ca261f841a2dbc9373ad51c671cc
m89p067 <92300319+m89p067@users.noreply.github.com>
2025-05-30 17:15:16 +0200

    Update bovine_for_ML.py

--- bovine_for_ML.py
+++ bovine_for_ML.py
@@ -4,7 +4,7 @@ from os import listdir
 from os.path import isfile, join
 import pdb
 
-mypath="D:/Parinaz/Bovine data for ML/"
+mypath="D:/Data/"
 appended_data = []
 onlyfiles = [f for f in listdir(mypath) if isfile(join(mypath, f))]
 
